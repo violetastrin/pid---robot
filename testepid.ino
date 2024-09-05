20257c9c69ccf5fdea7ec32a5a824aa36654d53b
Violeta Trindade <mvioleta.st@gmail.com>
2024-09-05 10:14:32 -0300

    Update testepid.ino

--- testepid.ino
+++ testepid.ino
@@ -2,26 +2,23 @@ kp = 0,2;
 kd = 0,2;
 error_passado = 0;
 
-while (true) {
+int leituraDireita = analogRead(REDIR);
+int leituraEsquerda = analogRead(RESQ);
 
-    int leituraDireita = analogRead(REDIR);
-    int leituraEsquerda = analogRead(RESQ);
+int err = leituraDireita - leituraEsquerda; // erro positivo -> robo a direita
+int d = err - error_passado;
 
-    int err = leituraDireita - leituraEsquerda; // erro positivo -> robo a direita
-    int d = err - error_passado;
-    
-    float pwme = 90 + vbase + err * kp + d*kd;
-    float pwmd = 90 - vbase + err * kp + d*kd;
-    
-    Serial.print(err);
-    Serial.print(",");    Serial.println(d);
+float pwme = 90 + vbase + err * kp + d*kd;
+float pwmd = 90 - vbase + err * kp + d*kd;
 
-    servodf.write(pwmd);
-    servodt.write(pwmd);
+Serial.print(err);
+Serial.print(",");    Serial.println(d);
 
-    servoef.write(pwme);
-    servoet.write(pwme);
+servodf.write(pwmd);
+servodt.write(pwmd);
+
+servoef.write(pwme);
+servoet.write(pwme);
 
 
-  error_passado = err;
-  }
+error_passado = err;
