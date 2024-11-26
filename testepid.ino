db50460ae9ba40072ff47d5a25dbfeefb989de60
Violeta Trindade <mvioleta.st@gmail.com>
2024-11-26 09:59:47 -0300

    Update testepid.ino

--- testepid.ino
+++ testepid.ino
@@ -1,31 +1,27 @@
 float kp;
   float kd;
-  float error_passado;
-  int vbase = 20;
-  error_passado = 0;
+  float error_passado = 0;
+  int vbase = 30;
 
   while (1) {
-    kp = 0, 5;
-    kd = 0, 5;
+    kp = 0.2;
+    kd = 0.5;
 
-    int leituraDireita = (analogRead(sensor[3]) - 400); 
-    int leituraEsquerda = (analogRead(sensor[1]) - 400); 
+    int leituraDireita = (analogRead(sensor[3])); 
+    int leituraEsquerda = (analogRead(sensor[1])); 
 
-    int err = leituraDireita - (leituraEsquerda);  // erro positivo -> robo a direita
+    int err = leituraEsquerda - (leituraDireita);  // erro positivo -> robo a direita
     int d = err - error_passado;
 
     float pwme = 90 + vbase + err * kp + d * kd;
     float pwmd = 90 - vbase + err * kp + d * kd;
 
-    Serial.print(err);
-    Serial.print(",");
-    Serial.println(d);
 
-    // servoDir_f.write(pwmd);
-    // servoDir_t.write(pwmd);
+    servoDir_f.write(pwmd);
+    servoDir_t.write(pwmd);
 
-    // servoEsq_f.write(pwme);
-    // servoEsq_t.write(pwme);
+    servoEsq_f.write(pwme);
+    servoEsq_t.write(pwme);
 
 
     error_passado = err;
@@ -33,7 +29,6 @@ float kp;
     Serial.print(pwme);
     Serial.print(" / ");
     Serial.println(pwmd); 
-    Serial.print(leituraEsquerda);
-    Serial.print(" / ");
-    Serial.println(leituraDireita); 
+    Serial.println(err);
+
   }
